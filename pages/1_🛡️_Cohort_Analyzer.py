e418b7aca08e359920469c9ae06679b6ce881ce2
cleecboevest <116837230+cleecboevest@users.noreply.github.com>
2023-05-02 09:39:08 -0700

    Update 1_🛡️_Cohort_Analyzer.py
    updated xlsxwriter

--- pages/1_🛡️_Cohort_Analyzer.py
+++ pages/1_🛡️_Cohort_Analyzer.py
@@ -29,7 +29,7 @@ def to_excel(df) -> bytes:
      output = io.BytesIO()
      writer = pd.ExcelWriter(output, engine="xlsxwriter")
      df.to_excel(writer, sheet_name="Sheet1")
-     writer.save()
+     writer.close()
      processed_data = output.getvalue()
      return processed_data
 
