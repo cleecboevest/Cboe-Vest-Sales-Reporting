31f5d0808435efdc2be77b8b7df93d33637391a7
cleecboevest <116837230+cleecboevest@users.noreply.github.com>
2024-01-26 12:24:05 -0800

    Update 📝_Sales_Reporting.py
    bug fix on the login form

--- 📝_Sales_Reporting.py
+++ 📝_Sales_Reporting.py
@@ -70,7 +70,7 @@ authenticator = stauth.Authenticate(
 )
      
    
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login('Login','main')
 
 if 'authentication_status' not in st.session_state:
      st.session_state['authentication_status'] = authentication_status
@@ -85,7 +85,7 @@ if authentication_status == None:
 if authentication_status == True:
 
      # Render logout button
-     authenticator.logout("Logout", "sidebar")
+     authenticator.logout('Logout', 'sidebar')
      st.sidebar.title(f"Welcome {name}!")
      
      #----------STATUS MESSAGE------
