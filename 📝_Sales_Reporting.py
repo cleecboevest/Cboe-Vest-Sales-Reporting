1fb3ffdbd9a39893bdb4ad1434307932c281e607
cleecboevest <116837230+cleecboevest@users.noreply.github.com>
2024-10-03 10:45:41 -0700

    Authenticator Updates
    fixed deprecated authenticator issues

--- 📝_Sales_Reporting.py
+++ 📝_Sales_Reporting.py
@@ -65,8 +65,7 @@ authenticator = stauth.Authenticate(
      config['credentials'],
      config['cookie']['name'],
      config['cookie']['key'],
-     config['cookie']['expiry_days'],
-     config['preauthorized']
+     config['cookie']['expiry_days']
 )
      
    
